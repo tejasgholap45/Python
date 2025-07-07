d382324b2e35d9de3bbf32e3697ed20f5230b9ae
Tejas Gholap <tejasgholap961@gmail.com>
2025-07-07 18:23:39 +0530

    addition.py

---
+++ addition.py
@@ -0,0 +1,8 @@
+def add_numbers(a, b):
+    return a + b
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+result = add_numbers(num1, num2)
+print("The sum is:", result)
