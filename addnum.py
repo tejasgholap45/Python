2a2c1e154b3d012336091717a8b18e4ffd61915e
Tejas Gholap <tejasgholap961@gmail.com>
2025-12-16 21:13:51 +0530

    Create addnum.py

---
+++ addnum.py
@@ -0,0 +1,4 @@
+num1 = float(input("Enter the first number for addition: "))
+num2 = float(input("Enter the second number for addition: "))
+sum_result = num1 + num2
+print(f"sum: {num1} + {num2} = {sum_result}")
